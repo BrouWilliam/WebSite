032a54c9a16af5086cd34a50b6704cda1ef33a7f
BrouWilliam <brouwilliam@hotmail.com>
2020-01-27 21:20:37 -0300

    alter homepage

--- js/start.js
+++ js/start.js
@@ -1,19 +1,19 @@
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  responsive:{
-    0:{
-      items:1
-    },
-    600:{
-      items:3
-    },
-    1000:{
-        items:5
-        }
-    }
-});
+// $('.owl-carousel').owlCarousel({
+//   loop:true,
+//   margin:10,
+//   nav:true,
+//   responsive:{
+//     0:{
+//       items:1
+//     },
+//     600:{
+//       items:3
+//     },
+//     1000:{
+//         items:5
+//         }
+//     }
+// });
 
 $(document).ready(function(){
     $("a").on('click', function(event) {
@@ -29,24 +29,33 @@ $(document).ready(function(){
   });
 });
 
-$(document).ready(function(){       
-    var scroll_start = 0;
-    var startchange = $('#startchange');
-    var offset = startchange.offset();
-    if (startchange.length){
-      $(document).scroll(function() { 
-        scroll_start = $(this).scrollTop();
-          if(scroll_start > offset.top) {
-            $('.navbar').css('opacity', '1');
-            $('.navbar').css('background-color', '#FFBB00');;
-            $('.navbar').css('transition', '.5s ease-in-out')
+// $(document).ready(function(){       
+//     var scroll_start = 0;
+//     var startchange = $('#startchange');
+//     var offset = startchange.offset();
+//     if (startchange.length){
+//       $(document).scroll(function() { 
+//         scroll_start = $(this).scrollTop();
+//           if(scroll_start > offset.top) {
+//             $('.navbar').css('opacity', '1');
+//             $('.navbar').css('background-color', '#FFBB00');;
+//             $('.navbar').css('transition', '.5s ease-in-out')
 
-          } 
-          else{
-            $('.navbar').css('opacity', '0');
-          }
+//           } 
+//           else{
+//             $('.navbar').css('opacity', '0');
+//           }
           
-    });
+//     });
+//   }
+// });
+
+$(window).on('scroll', function(){
+  if($(window).scrollTop()){
+      $('nav').addClass('black');
   }
-});
+  else{
+      $('nav').removeClass('black');
+  }
+})
 
