f40ab050090b55d61ae2be04329a54dad5e93952
BrouWilliam <brouwilliam@hotmail.com>
2021-07-29 13:55:32 -0300

    portfoliozada

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
-function typeWriter(elemento) {
-  const textoArray = elemento.innerHTML.split('');
-  elemento.innerHTML = '';
-  textoArray.forEach((letra, i) => {
-    setTimeout(() => elemento.innerHTML += letra, 155 * i);
-  });
-};
+// **Titulo maquina de escrever
 
-  const titulo = document.querySelector('h1');
-  typeWriter(titulo);
+// function typeWrite(elemento) {
+//   const textoArray = elemento.innerHTML.split('')
+//   elemento.innerHTML = ''
+
+//   textoArray.forEach((letra, i) => {
+//       setTimeout(() => elemento.innerHTML += letra, 75 * i)
+//   })
+
+// }
+
+// const titulo = document.querySelector('h1')
+// typeWrite(titulo)
+
+
+
+
+// ** Idade
+var ano_atual = new Date().getFullYear();
+        var ano_informado = 1999;
+        var idade = (ano_atual - ano_informado);
+
+        console.log(idade)
+
+
+        var p1 = window.document.getElementById('idade');
+        p1.innerText = idade
+
+
+
+
+
+// ** Contagem projetos
 
